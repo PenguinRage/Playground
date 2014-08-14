ffa3433a4affa4edb4a622b61b28bd985bb31d3a
PenguinRage <Inc2En@hotmail.com>
2014-08-14 23:20:09 +1000

    Cannot instantiate the type MyNode

---
+++ Datastructure/workspace/LinkedList/Linkedlist/LinkedList.java
@@ -0,0 +1,12 @@
+
+public interface LinkedList {
+	public Node getHead();
+
+    public int size();
+    public boolean isEmpty();
+
+    public Object get(int i);
+
+    public void add(Object o);
+    public void remove(Object o);
+}
